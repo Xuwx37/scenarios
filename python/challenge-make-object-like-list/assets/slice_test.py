9a15ae44a53040cc33c1a0fb1b2e48013952ce57
Xu wenxuan <1048501553@qq.com>
2023-03-08 11:02:29 +0800

    add challenge-make-object-like-list

---
+++ python/challenge-make-object-like-list/assets/slice_test.py
@@ -0,0 +1,21 @@
+import unittest
+from magic_list import MagicList
+
+class TestMagicList(unittest.TestCase):
+    def setUp(self):
+        self.magic_list = MagicList(1, 2, 3)
+
+    def test_getitem(self):
+        self.assertEqual(self.magic_list[0], 1)
+        self.assertEqual(self.magic_list[1], 2)
+        self.assertEqual(self.magic_list[2], 3)
+
+    def test_setitem(self):
+        self.magic_list[0] = 4
+        self.assertEqual(self.magic_list[0], 4)
+        self.magic_list[1:3] = [5, 6]
+        self.assertEqual(self.magic_list[1], 5)
+        self.assertEqual(self.magic_list[2], 6)
+
+if __name__ == '__main__':
+    unittest.main()
