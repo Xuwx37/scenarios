cd22a344df0130ab7ee700e72b46302a37df8bc0
Xu wenxuan <1048501553@qq.com>
2023-04-25 16:51:23 +0800

    update validate_user_input_test.py

--- python/challenge-building-a-simple-calculator/assets/validate_user_input_test.py
+++ python/challenge-building-a-simple-calculator/assets/validate_user_input_test.py
@@ -7,12 +7,21 @@ from simple_calculator import simple_calculator
 
 class TestStep2(unittest.TestCase):
     def test_valid_input(self):
-        result = simple_calculator().validate_user_input("10", "5", "+")
-        self.assertEqual(result, None)
+        result1 = simple_calculator().validate_user_input("10", "5", "+")
+        result2 = simple_calculator().validate_user_input("10", "5", "-")
+        result3 = simple_calculator().validate_user_input("10", "5", "*")
+        result4 = simple_calculator().validate_user_input("10", "5", "/")
+        self.assertEqual(result1, None)
+        self.assertEqual(result2, None)
+        self.assertEqual(result3, None)
+        self.assertEqual(result4, None)
 
     def test_invalid_input(self):
-        result = simple_calculator().validate_user_input("abc", "def", "@")
-        self.assertEqual(result, "Please enter valid numbers")
+        result1 = simple_calculator().validate_user_input("abc", "def", "@")
+        result2 = simple_calculator().validate_user_input("2", "3", "@")
+        self.assertEqual(result1, "Please enter valid numbers")
+        self.assertEqual(result2, "Please enter valid operation")
+
 
 
 if __name__ == "__main__":
