b55430d6ba79c1851d7e006d089111e298fa9225
Xu wenxuan <1048501553@qq.com>
2023-04-25 15:56:49 +0800

    add challenge-building-a-simple-calculator

---
+++ python/challenge-building-a-simple-calculator/assets/validate_user_input_test.py
@@ -0,0 +1,16 @@
+import unittest
+from simple_calculator import simple_calculator
+
+
+class TestStep2(unittest.TestCase):
+    def test_valid_input(self):
+        result = simple_calculator().validate_user_input("10", "5", "+")
+        self.assertEqual(result, None)
+
+    def test_invalid_input(self):
+        result = simple_calculator().validate_user_input("abc", "def", "@")
+        self.assertEqual(result, "Please enter valid numbers")
+
+
+if __name__ == "__main__":
+    unittest.main()
